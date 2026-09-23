3e483a0f170d1ae6705f940cb295652840e37b35
wistermarquez90-oss <wistermarquez90@gmail.com>
2026-09-23 13:02:48 -0400

    git

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  // Tus datos exactos de GitHub Pages
+  site: 'https://wistermarquez90-oss.github.io',
+  base: '/presentation-dashboard',
+  
   integrations: [react(), tailwind()]
 });
